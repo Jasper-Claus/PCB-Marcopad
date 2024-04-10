c462b3e4624b032c476a2cbc8e0c4edb094f80fb
Jasper Claus <97516104+Jasper-Claus@users.noreply.github.com>
2024-04-10 23:51:16 +0200

    Aktualisiere Bibliothekspfade in .vscode/settings.json und ändere Pin-Nummer für Button in main.py

--- main.py
+++ main.py
@@ -2,8 +2,15 @@
 from machine import Pin
 import time
 
+# led = Pin(25, Pin.OUT)
+
+# while True:
+#     led.toggle()  # Wechselt den Zustand der LED
+#     time.sleep(1)  # Wartet 1 Sekunde
+    
+    
 # Definieren Sie Pin 2 als Eingang und aktivieren Sie den internen Pull-up-Widerstand
-button = Pin(2, Pin.IN, Pin.PULL_UP)
+button = Pin(7, Pin.IN, Pin.PULL_UP)
 
 while True:
     # Wenn der Button gedrückt wird (LOW), sende eine Nachricht über die serielle Verbindung
@@ -12,3 +19,4 @@ while True:
         # Warte eine Weile, um Prellen zu vermeiden
         time.sleep(0.2)
         
+
